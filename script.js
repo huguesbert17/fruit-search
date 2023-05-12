f935b6b43fa8e3e8f040b5cce27444220a8484c5
Hugues Ebert M <huguesbert17@gmail.com>
2023-05-12 14:18:27 -0400

    All done

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	results = fruit.filter(fr => {
+		return fr.toLowerCase().indexOf(str.toLowerCase()) > -1
+	});
+	return results;
+}
+
+function searchHandler(e) {
+	if (e.target.value === "") return
+	const text = e.target.value.trim().toLowerCase()
+	suggestions.style.cssText = "display: block"
+	showSuggestions(search(text), text);
+}
+
+function showSuggestions(results, inputVal) {
+	let li = ""
+	for(const index in results){
+		li += "<li>" + results[index] + "</li>";
+	}
+	suggestions.innerHTML = li;
+}
+
+function useSuggestion(e) {
+	input.value = e.target.innerText
+	suggestions.style.cssText = "display: none"
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
